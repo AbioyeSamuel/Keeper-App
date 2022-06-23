5faa6c15a89ed13830419146d758bd7232a4be3b
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-23 17:48:09 +0100

    Keeper App Part 2. Worked on the note section of the app. Introducing Arrow function, map method of looping through notes array and react properties

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note noteTitle={note.title} noteContent={note.content} />
+      ))}
       <Footer />
     </div>
   );
